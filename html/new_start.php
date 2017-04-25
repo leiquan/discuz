0666f97806f11b3e0d969fd96ca5172924c4061d
leiquan <leiquan@live.com>
2017-04-26 02:33:57 +0800

    Uuuu

--- html/new_start.php
+++ html/new_start.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once '../source/class/class_core.php';//必须的，你懂的
+require_once '../source/class/class_core.php';//����ģ��㶮��
 
-$discuz = & discuz_core::instance();//必须的，你懂的
+$discuz = & discuz_core::instance();//����ģ��㶮��
 
-$discuz->cachelist = $cachelist;//必须的，你懂的
+$discuz->cachelist = $cachelist;//����ģ��㶮��
 
-$discuz->init();//必须的，你懂的
+$discuz->init();//����ģ��㶮��
 
 ?>
 
@@ -16,10 +16,10 @@ $discuz->init();//必须的，你懂的
 <head>
   <meta charset="gb2312">
 
-  <title>雷全的个人网站</title>
-  <meta name="keywords" content="雷全,雷全的个人网站,雷全的个人主页,雷全的博客,leiquan,leiquan.me,leiquan.website">
+  <title>��ȫ�ĸ�����վ</title>
+  <meta name="keywords" content="��ȫ,��ȫ�ĸ�����վ,��ȫ�ĸ�����ҳ,��ȫ�Ĳ���,leiquan,leiquan.me,leiquan.website">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <meta name="description" content="欢迎访问雷全的个人网站，这里有我的技术博客、打油小诗、随笔杂文、读书笔记以及我分享的照片和资源。">
+  <meta name="description" content="��ӭ������ȫ�ĸ�����վ���������ҵļ������͡�����Сʫ��������ġ�����ʼ��Լ��ҷ�������Ƭ����Դ��">
 
   <link rel="stylesheet" href="css/style.css">
 
@@ -33,7 +33,7 @@ $discuz->init();//必须的，你懂的
 
   <div class="container">
 
-    <div class='title' >欢迎加入一亩三分地论坛</div>
+    <div class='title' >��ӭ����һĶ���ֵ���̳</div>
 
     <div class="step">
 
@@ -53,53 +53,53 @@ $discuz->init();//必须的，你懂的
       <?php
       echo $_G['username'];
       ?>
-       , 恭喜你成功注册一亩三分地！
+       , ��ϲ��ɹ�ע��һĶ���ֵأ�
     </div>
 
     <div class="brand small">
-      你目前的会员等级是
-      <span>新农上路</span>
+      ��Ŀǰ�Ļ�Ա�ȼ���
+      <span>��ũ��·</span>
     </div>
 
-    <div class="now">你现在可以：</div>
+    <div class="now">�����ڿ��ԣ�</div>
 
     <div class="box">
 
       <div class="item">
-        <a class="button" href='http://leiquan.me/home.php?mod=spacecp&ac=avatar'>设置头像</a>
-        <div class="txt">上传个性化的头像得10积分</div>
+        <a class="button" href='http://leiquan.me/home.php?mod=spacecp&ac=avatar'>����ͷ��</a>
+        <div class="txt">�ϴ����Ի���ͷ���10����</div>
       </div>
 
       <div class="item">
-        <a class="button" href='http://leiquan.me/forum.php?mod=forumdisplay&fid=2'>新手报道</a>
-        <div class="txt">快去跟大家打个招呼吧</div>
+        <a class="button" href='http://leiquan.me/forum.php?mod=forumdisplay&fid=2'>���ֱ���</a>
+        <div class="txt">��ȥ����Ҵ���к���</div>
       </div>
 
       <div class="item">
-        <a class="button" href='https://www.takethegre.com/cn/gateway/scoreselect/'>GRE在线报名</a>
-        <div class="txt">立刻进行GRE报名</div>
+        <a class="button" href='https://www.takethegre.com/cn/gateway/scoreselect/'>GRE���߱���</a>
+        <div class="txt">���̽���GRE����</div>
       </div>
 
       <div class="item">
-        <a class="button" href='http://leiquan.me/home.php?mod=space&uid=2'>个人中心</a>
-        <div class="txt">论坛热帖</div>
+        <a class="button" href='http://leiquan.me/home.php?mod=space&uid=2'>��������</a>
+        <div class="txt">��̳����</div>
       </div>
 
     </div>
 
     <div class="box-left">
-      <div class="head">热版</div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
+      <div class="head">�Ȱ�</div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
       <div class="bottom"></div>
 
-      <div class="head">美国版</div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
-      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>美国留学</a></div>
+      <div class="head">������</div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
+      <div class="line"><a href='http://www.1point3acres.com/bbs/forum.php?mod=guide'>������ѧ</a></div>
       <div class="bottom"></div>
 
 

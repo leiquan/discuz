23d428e9f2f66cb16326b33c38c06159369bf32a
leiquan <leiquan@live.com>
2017-05-01 22:16:38 +0800

    添加CSS

--- start.php
+++ start.php
@@ -1,9 +1,9 @@
 <?php
 
-require './source/class/class_core.php'; // 引入系统核心文件
-$discuz = & discuz_core::instance(); // 以下代码为创建及初始化对象
+require './source/class/class_core.php';
+$discuz = & discuz_core::instance();
 $discuz->cachelist = $cachelist;
 $discuz->init();
 
-include template('start'); // 调用单页模版文件
+include template('start');
 ?>

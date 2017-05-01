bb22172656579fcd930e94ca9c6d7bd05faf5087
leiquan <leiquan@live.com>
2017-05-01 22:07:01 +0800

    up

--- host.php
+++ host.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../source/class/class_core.php';//引入系统核心文件
+require './source/class/class_core.php';//引入系统核心文件
 $discuz = & discuz_core::instance();//以下代码为创建及初始化对象
 $discuz->cachelist = $cachelist;
 $discuz->init();

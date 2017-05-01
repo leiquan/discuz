f16645d3a0f99e107cc041c4f327a893f2131b58
leiquan <leiquan@live.com>
2017-05-01 22:42:04 +0800

    更新

--- 1point3acres/host.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require '../source/class/class_core.php';//引入系统核心文件
-$discuz = & discuz_core::instance();//以下代码为创建及初始化对象
-$discuz->cachelist = $cachelist;
-$discuz->init();
-
-include template('host');//调用单页模版文件
-?>

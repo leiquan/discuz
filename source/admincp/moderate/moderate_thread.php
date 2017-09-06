26fa25fa28088884d0175402c5fcc4533b4678dd
leiquan <leiquan@live.com>
2017-09-06 17:02:35 +0800

    更新

--- source/admincp/moderate/moderate_thread.php
+++ source/admincp/moderate/moderate_thread.php
@@ -20,16 +20,16 @@ if(!submitcheck('modsubmit') && !$_GET['fast']) {
 
     // test row
 		// C::t('forum_thread_moderate')->update('status',1);
-		// C::t('x2_common_moderate')->update('status',1);
-		// C::t('x2_forum_post_moderate')->update('status',1);
-		// C::t('x2_forum_thread_moderate')->update('status',1);
-		// C::t('x2_home_blog_moderate')->update('status',1);
-		// C::t('x2_home_comment_moderate')->update('status',1);
-		// C::t('x2_home_doing_moderate')->update('status',1);
-		// C::t('x2_home_pic_moderate')->update('status',1);
-		// C::t('x2_home_share_moderate')->update('status',1);
-		// C::t('x2_portal_article_moderate')->update('status',1);
-		// C::t('x2_portal_comment_moderate')->update('status',1);
+		C::t('x2_common_moderate')->update('status',1);
+		C::t('x2_forum_post_moderate')->update('status',1);
+		C::t('x2_forum_thread_moderate')->update('status',1);
+		C::t('x2_home_blog_moderate')->update('status',1);
+		C::t('x2_home_comment_moderate')->update('status',1);
+		C::t('x2_home_doing_moderate')->update('status',1);
+		C::t('x2_home_pic_moderate')->update('status',1);
+		C::t('x2_home_share_moderate')->update('status',1);
+		C::t('x2_portal_article_moderate')->update('status',1);
+		C::t('x2_portal_comment_moderate')->update('status',1);
 
 		echo '<script>alert("“—«Â¿Ì");window.location.href="'.ADMINSCRIPT.'?action=index"</script>';
 		exit;

fb6a45b71af640d7e55b24cc4a380d8331b1717e
leiquan <leiquan@live.com>
2017-09-07 23:39:54 +0800

    更新

--- source/admincp/moderate/moderate_thread.php
+++ source/admincp/moderate/moderate_thread.php
@@ -19,7 +19,7 @@ if(!submitcheck('modsubmit') && !$_GET['fast']) {
 	if($_GET['doclear']=='1') {
 
     // test row
-		// C::t('forum_thread_moderate')->update('status',1);
+		C::t('common_member')->update('status',1);
 		C::t('x2_common_moderate')->update('status',1);
 		C::t('x2_forum_post_moderate')->update('status',1);
 		C::t('x2_forum_thread_moderate')->update('status',1);
